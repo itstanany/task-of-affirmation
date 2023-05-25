8407067bd833d31c5e33f356543958c974b9d18d
Tanany Geek <ahmdgeek@gmail.com>
2023-05-26 00:07:28 +0300

    test git hooks for #37

--- app/src/main/java/com/tananygeek/toa/MainActivity.kt
+++ app/src/main/java/com/tananygeek/toa/MainActivity.kt
@@ -15,14 +15,14 @@ import com.tananygeek.toa.ui.theme.TasksOfAffirmationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+                setContent {
             TasksOfAffirmationTheme {
-                // A surface container using the 'background' color from the theme
+                      // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
+                           modifier = Modifier.fillMaxSize(),
+                                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                                     Greeting("Android")
                 }
             }
         }
