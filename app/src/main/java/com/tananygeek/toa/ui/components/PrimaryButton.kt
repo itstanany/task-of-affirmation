dee8163f87fe5055a00a48cc7824d8f5d5c39e3b
Tanany Geek <ahmdgeek@gmail.com>
2023-05-30 19:41:46 +0300

    adding primary button for #24

---
+++ app/src/main/java/com/tananygeek/toa/ui/components/PrimaryButton.kt
@@ -0,0 +1,53 @@
+package com.tananygeek.toa.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import com.tananygeek.toa.R
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.primary,
+) {
+    val buttonColors = buttonColors(
+        containerColor = backgroundColor,
+    )
+    Button(
+        onClick = onClick,
+        colors = buttonColors,
+        shape = RoundedCornerShape(50),
+        modifier = modifier
+            .height(dimensionResource(id = R.dimen.button_height))
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = text.toUpperCase(Locale.current),
+
+        )
+    }
+}
+
+@Preview(
+    name = "Night Mode",
+)
+@Composable
+fun PrimaryButtonPreview() {
+    PrimaryButton(
+        text = "Primary Button",
+        onClick = { },
+    )
+}
