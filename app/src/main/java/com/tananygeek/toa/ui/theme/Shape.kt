6bd542d614fd18aa53f142671576cd8796c3448f
Tanany Geek <ahmdgeek@gmail.com>
2023-05-31 11:18:23 +0300

    Add text field input for #42

--- app/src/main/java/com/tananygeek/toa/ui/theme/Shape.kt
+++ app/src/main/java/com/tananygeek/toa/ui/theme/Shape.kt
@@ -3,3 +3,4 @@ package com.tananygeek.toa.ui.theme
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 val ButtonShape = RoundedCornerShape(50)
+val TextFieldShape = RoundedCornerShape(50)
